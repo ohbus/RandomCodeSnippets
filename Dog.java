907bb4e681c9100e31e3054a4524ddd8088903cc
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2019-07-28 13:38:05 +0530

    update

---
+++ Dog.java
@@ -0,0 +1,17 @@
+class Dog {
+    //Instance Variable
+    String name, color;
+    int size;
+
+    Dog()   {
+        name =  "";
+        color = "";
+        size = 0;
+    }   //Default Const.
+
+    Dog(String n, String c, int s)  {
+        name = n;
+        color = c;
+        size = s;
+    }   //Paramertrized Const.
+}
