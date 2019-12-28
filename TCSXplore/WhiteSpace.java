61fef3bbfb0c6b9a246f713e8b37b26a1c82211b
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2019-12-28 23:57:52 +0530

    Xplore

---
+++ TCSXplore/WhiteSpace.java
@@ -0,0 +1,18 @@
+import java.util.Scanner ;
+
+public class WhiteSpace {
+
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in) ;
+
+        String s = sc.nextLine() ;
+        int i , space = 0 , len = s.length() ;
+
+        for( i = 0 ; i < len ; i++ )    
+            if (s.charAt(i) == ' ')
+                space++;
+        
+        System.out.println("Characters : "+(len-space)+" Spaces : "+space) ;
+    }
+
+}
